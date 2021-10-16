765ee0f7453d2c8a3d3c544c2c4ab4b8b21ce7e6
abdelrahman <a.mostafahsn@gmail.com>
2021-10-16 17:21:18 +0200

    last update

--- app/src/main/java/amhsn/admin/MainActivity.kt
+++ app/src/main/java/amhsn/admin/MainActivity.kt
@@ -1,9 +1,18 @@
 package amhsn.admin
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(newBase)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
